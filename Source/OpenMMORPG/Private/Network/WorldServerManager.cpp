44f452e3053b5354434e238bb14a35b05fabed21
Alex Sinelnikov <sinelniko@gmail.com>
2020-09-28 21:53:00 +0300

    Small fix

--- Source/OpenMMORPG/Private/Network/WorldServerManager.cpp
+++ Source/OpenMMORPG/Private/Network/WorldServerManager.cpp
@@ -1,8 +1,8 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
-#include "OpenMMORPG/Public/Network/WorldServerEntity.h"
 #include "OpenMMORPG/Public/Network/WorldServerManager.h"
+#include "OpenMMORPG/Public/Network/WorldServerEntity.h"
+
 
 TArray<UWorldServerEntity*> UWorldServerManager::ServerList;
 
